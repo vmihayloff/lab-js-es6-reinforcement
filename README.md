0b9df5a7a95bc14213dd87ef4e864c30c9bbc269
sandrabosk <sandraboskovic@hotmail.com>
2020-01-28 17:26:26 -0500

    add instructions, separate data and iterations

--- README.md
+++ README.md
@@ -1,57 +1,90 @@
-# JS | ES 6 - reinforcement
+![logo_ironhack_blue 7](https://user-images.githubusercontent.com/23629340/40541063-a07a0a8a-601a-11e8-91b5-2f13e4e6b441.png)
 
-<br>
+# JS | ES6 - reinforcement
 
-## `for...of` Bank - Exercise
-
-<br>
+<!-- ## `for...of` Bank - Exercisem-->
 
 ![](https://media.giphy.com/media/3o6ZtpoNGDS4cZ8GWY/giphy.gif)
 
-<br>
+## Introduction
 
-For this exercise you can use [Repl.it](https://repl.it/) or your code editor.
+For this exercise, you can use [Repl.it](https://repl.it/) or your code editor.
 
-Using the below file `index.js` finish the tasks as specified in the task list.
+Using the array of users given in the **`data.js`** file, go ahead and finish the tasks as specified in the task list. You should have your solution saved in the **`index.js`** file.
 
-<br>
+## Requirements
 
-#### Task 1
+- Fork this repo
+- Then clone this repo.
 
-Using the [`for...of`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of) loop iterate over the `usersArray` , access each object via [`dot notation`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Property_accessors), create a string containing the full name using the [ES6 template literals - back ticks](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals).
+## Submission
 
-Push each new full name string into a new array `userNames`
+- Upon completion, run the following commands:
 
-<br>
+```shell
+$ git add .
+$ git commit -m "done"
+$ git push origin master
+```
 
-#### Task 2
+- Create Pull Request so your TAs can check up your work.
 
-Using `for...of` loop iterate over the `usersArray` and push all the first names into a new array `userFirstNames`.
+## Instructions
 
-<br>
+### Iteration 1: `for...of` loop
 
-#### Task 3
+Create a function `getFirstNames()` and, using [for...of](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of) loop, iterate over the _usersArray_ and push all the first names into a new array `userFirstNames`.
 
-Using `for...of` loop iterate over the `usersArray` push all of the last names into a new array `userLastNames`.
+### Iteration 2: `for...of` loop and ES6 string literals `${}`
 
-<br>
+Create a function `getFullNames()` which will return an array of strings where each string will represent the full name of each user from the _usersArray_.
 
-**Task 4** -
+**Steps**:
 
-Using `for...of` loop iterate over the `usersArray` and:
+1. using [for...of](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of) loop iterate over the _usersArray_ and
+2. using [ES6 string interpolation](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals) create a string containing full name (_tip_: access each object via [`dot notation`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Property_accessors)),
+3. push that string into a new array `userFullNames`.
 
-1. [Destructure](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#Object_destructuring) each user object's `firstName`, `lastName` and `balance`, to create new variables.
+### Iteration 3: ES6 destructuring, `for of loop`, object literal
 
-2. After destructuring , use those new variables to create a new object using object literal (only name of the variable) consisting of `firstName`, `lastName` and `balance` , example:
+Create a function `getUsersCreditDetails()` to grab each user's first and last name and their balance.
 
-   ```js
-   const userCreditDetails = {
-     firstName,
-     lastName,
-     balance
-   };
-   ```
+**Steps**:
 
-3. push each new object into a new array `userCreditDetails`
+1. Using `for...of` loop iterate over the `usersArray`;
+2. [Destructure](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#Object_destructuring) `firstName`, `lastName` and `balance` from each user object to create new variables;
+3. After destructuring create a new object using object literal (only name of variable) consisting of `firstName`, `lastName` and `balance`. Check the following example:
 
-<br>
+```js
+const userDetails = {
+  firstName,
+  lastName,
+  balance
+};
+```
+
+4. Push each new object into a new array `usersCreditDetails`
+
+### Iteration 4: practice `.filter()` method and how to return two elements
+
+Create a function `genderView()` which should return two arrays new arrays `femaleUsers` and `maleUsers`. Depending on the gender, fill them with strings containing users' first and last names.
+
+### Bonus - Iteration 5
+
+Create a function `genderCount()` that should use the returned object from the previous function and print how many females and how many male users there are.
+
+Check the `index.js` to see what should be expected output.
+
+### Bonus - Iteration 6
+
+Create a function `promo20()` to find all users whose balance is greater than 20000 and send them a personalized message offering some promotion.
+
+Check the `index.js` to see what should be expected output.
+
+### Bonus - Iteration 7
+
+Create a function `addActive()` which should loop through the `usersArray` and add a new property `isActive` with a value `true` to all users (hint: each user in the `usersArray` is represented with object and you could use `...` with objects :wink: ).
+
+Check the `index.js` to see what should be expected output.
+
+**Happy coding!** :heart:
--- index.js
+++ index.js
@@ -1,114 +1,110 @@
-const usersArray = [
-  {
-    firstName: "Kirby",
-    lastName: "Doyle",
-    id: "b71794e5-851e-44b5-9eec-1dd4e897e3b8",
-    isActive: false,
-    balance: "$3,570.06",
-    gender: "male"
-  },
-  {
-    firstName: "Tracie",
-    lastName: "May",
-    id: "1af0e9ee-66fc-4298-b8ce-5d99bcbaac05",
-    isActive: false,
-    balance: "$1,547.73",
-    gender: "female"
-  },
-  {
-    firstName: "Kendra",
-    lastName: "Hines",
-    id: "5e92af3a-b08e-4689-bdeb-3226300470e3",
-    isActive: true,
-    balance: "$12,383.08",
-    gender: "female"
-  },
-  {
-    firstName: "Kinney",
-    lastName: "Howard",
-    id: "0ad2388d-83e1-4831-9cc4-e3581f8edf36",
-    isActive: false,
-    balance: "$3,207.06",
-    gender: "male"
-  },
-  {
-    firstName: "Howard",
-    lastName: "Gilmore",
-    id: "0719205d-c965-44cb-a128-708cf335b26c",
-    isActive: true,
-    balance: "$21,307.75",
-    gender: "male"
-  },
-  {
-    firstName: "Rachelle",
-    lastName: "Schneider",
-    id: "04012184-651b-41eb-9642-d362fedff02f",
-    isActive: true,
-    balance: "$35,121.49",
-    gender: "female"
-  },
-  {
-    firstName: "Lizzie",
-    lastName: "Alford",
-    id: "598ca7ec-888e-494d-ae94-c21ace3ffa52",
-    isActive: false,
-    balance: "$4,382.94",
-    gender: "female"
+// ***************************************************************************
+// Iteration 1 - `for...of` loop
+// ***************************************************************************
+
+const getFirstNames = arr => {
+  const userFirstNames = [];
+  for (let user of arr) {
+    // Your code goes here ...
   }
-];
+};
 
+getFirstNames(usersArray);
+// expected output:
+// [ 'Kirby', 'Tracie', 'Kendra', 'Kinney', 'Howard', 'Rachelle', 'Lizzie' ]
 
-// TASK 1 - `for...of` loop and ES6 string literals `${}`
-// Using `for...of` loop iterate over the usersArray and 
-// 1. using ES 6 string interpolation create a string containing full name
-// 2. push that string into a new array `userNames`
+// ***************************************************************************
+// Iteration 2 - `for...of` loop and ES6 string literals `${}`
+// ***************************************************************************
 
-const userNames = [];
-for (let user of usersArray) {
-  // Your Code Here ...
-}
+const getFullNames = arr => {
+  // Your code goes here ...
+};
 
-console.log('userNames', userNames);
+getFullNames(usersArray);
+// expected output:
+// [ 'Kirby Doyle', 'Tracie May', 'Kendra Hines', 'Kinney Howard',
+//   'Howard Gilmore', 'Rachelle Schneider', 'Lizzie Alford' ]
 
+// ***************************************************************************
+// Iteration 3 - ES6 destructuring , for of loop, object literal
+// ***************************************************************************
 
+const getUsersCreditDetails = arr => {
+  // Your code goes here ...
+};
 
-//  TASK 2 - for of loop
+getUsersCreditDetails(usersArray);
+// expected output:
+// [ { firstName: 'Kirby', lastName: 'Doyle', balance: '$3,570.06' },
+// { firstName: 'Tracie', lastName: 'May', balance: '$1,547.73' },
+// { firstName: 'Kendra', lastName: 'Hines', balance: '$12,383.08' },
+// { firstName: 'Kinney', lastName: 'Howard', balance: '$3,207.06' },
+// { firstName: 'Howard', lastName: 'Gilmore', balance: '$21,307.75' },
+// { firstName: 'Rachelle', lastName: 'Schneider', balance: '$35,121.49' },
+// { firstName: 'Lizzie', lastName: 'Alford', balance: '$4,382.94' } ]
 
-// Using `for...of` loop iterate over the `usersArray` 
-// 1. push all first names into a new array `userFirstNames
-const userFirstNames = [];
-for (let user of usersArray) {
-  // Your Code Here ...
-}
+// ***************************************************************************
+// Iteration 4 - practice `.filter()` method and how to return two elements
+// ***************************************************************************
 
-console.log('userFirstNames', userFirstNames);
+const genderView = users => {
+  // Your code goes here ...
+};
 
+genderView(usersArray);
+// expected output:
+// {
+//    femaleUsers: [ 'Tracie May', 'Kendra Hines', 'Rachelle Schneider', 'Lizzie Alford' ],
+//    maleUsers: [ 'Kirby Doyle', 'Kinney Howard', 'Howard Gilmore' ]
+// }
 
+// ***************************************************************************
+// Bonus - Iteration 5
+// ***************************************************************************
 
-//  TASK 3 - for of loop
+const data = genderView(usersArray);
 
-// Using `for...of` loop iterate over the `usersArray` 
-// 1. push all of the last names into a new array `userLastNames
-const userLastNames = [];
-for (let user of usersArray) {
-     // Your Code Here ...
-}
-console.log('userLastNames', userLastNames);
+const genderCount = data => {
+  // Your code goes here ...
+};
 
+genderCount(data);
+// expected output:
+// Female: 4
+// Male: 3
 
+// ***************************************************************************
+// Bonus - Iteration 6
+// ***************************************************************************
 
+const promo20 = users => {
+  // Your code goes here ...
+};
 
-//  TASK 4 - ES6 destructuring , for of loop, object literal (variable name as a property name and value)
+// expected output:
+// Dear Howard, since your balance is $21,307.75, you are eligible to apply for this awesome credit card.
+// Dear Rachelle, since your balance is $35,121.49, you are eligible to apply for this awesome credit card.
 
-// Using `for...of` loop iterate over the `usersArray` 
-// 1. destructure `firstName`, `lastName` and `balance` from each user
-// 2. After destructuring create a new object using object literal (only name of variable) consisting of `firstName`, `lastName` and `balance` and 
-// 3. push each new object into a new array `userCreditDetails`
-destructuring
-const userCreditDetails = [];
+// ***************************************************************************
+// Bonus - Iteration 7
+// ***************************************************************************
 
-for (const user of usersArray) {
-  // Your Code Here ...
-}
+const addActive = users => {
+  // Your code goes here ...
+};
 
-console.log('userCreditDetails', userCreditDetails);
+addActive(usersArray);
+// expected output:
+// [
+//    { firstName: 'Kirby',
+//      lastName: 'Doyle',
+//      id: 'b71794e5-851e-44b5-9eec-1dd4e897e3b8',
+//      isActive: true,
+//      balance: '$3,570.06',
+//      gender: 'male'
+//    },
+//    {
+//      // ...
+//    }
+// ]
